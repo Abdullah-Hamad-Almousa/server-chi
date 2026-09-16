b08569cb37a40a63af9b343e491dcc9f440b9e5e
Abdullah almousa <abdullahmlwork@gmail.com>
2026-09-16 15:35:03 +0300

    commit first one new

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	server := http.Server{
+		Addr:    ":3000",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Failed to start the server", err)
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	write, err := w.Write([]byte("Hello Nerds!"))
+	if err != nil {
+		fmt.Println("Failed to write the response", err)
+	}
+	log.Println(write)
 }
